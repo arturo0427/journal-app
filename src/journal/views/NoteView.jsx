8582bc3dea318bed0953b817874fe2085b5301da
Arturo Muñoz <boomer_rap@hotmail.com>
2024-01-05 19:53:01 -0500

    Mostrar imagenes

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -113,7 +113,7 @@ export const NoteView = () => {
                 />
             </Grid>
 
-            <ImageGallery />
+            <ImageGallery images={note.imageUrls} />
 
         </Grid>
     )
